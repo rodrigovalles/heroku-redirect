d435f83404872ec4be2ac7123bf573c96b0cbfcc
rodrigovalles <vallesrodrigom@gmail.com>
2022-01-03 22:08:30 -0300

    mock server

--- web.js
+++ web.js
@@ -3,5 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({ target: 'https://api.triunfonet.com.ar', changeOrigin: true }));
+//app.use('/', createProxyMiddleware({ target: 'https://api.triunfonet.com.ar', changeOrigin: true }));
+app.use('/', (req, res) => {
+  res.json({hola: true})
+})
 app.listen(5000);
