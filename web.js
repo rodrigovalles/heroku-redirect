c7cc674d5e19aa0876de16b8fb1b610efdffce0e
rodrigovalles <vallesrodrigom@gmail.com>
2022-01-03 22:20:34 -0300

    redrect

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL = process.env.NEW_BASE_URL || 'https://api.triunfonet.com.ar';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
-app.use('/custom-check', (req, res) => {
+app.use('/ping', (req, res) => {
   res.json({hola: true})
 })
 
-app.get('*', function(request, response) {
-  response.redirect(redirectStatus, newBaseURL + request.url);
-});
+app.use('*', createProxyMiddleware({ target: 'https://api.triunfonet.com.ar', changeOrigin: true }));
+
+
+// app.get('*', function(request, response) {
+//   response.redirect(redirectStatus, newBaseURL + request.url);
+// });
 
 app.listen(port, function() {
   console.log("Listening on " + port);
