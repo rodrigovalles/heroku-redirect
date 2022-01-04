36e1d2c20f56a07f272b3123e25e48387c2e5673
rodrigovalles <vallesrodrigom@gmail.com>
2022-01-03 22:15:49 -0300

    mandale mecha

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+var express = require('express');
+var app = express();
 
-const app = express();
+var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
+var port = process.env.PORT || 5000;
 
-//app.use('/', createProxyMiddleware({ target: 'https://api.triunfonet.com.ar', changeOrigin: true }));
-app.use('/', (req, res) => {
+app.use('/custom-check', (req, res) => {
   res.json({hola: true})
 })
-app.listen(5000);
+
+app.get('*', function(request, response) {
+  response.redirect(redirectStatus, newBaseURL + request.url);
+});
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
